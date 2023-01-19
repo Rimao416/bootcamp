fa86c0e491510b2aac920188a9ce4fb8fe2d080a
Rimao416 <omarkayumba12345@gmail.com>
2023-01-19 16:03:00 +0100

    Premier moyen de lecture dans un fichier

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const util = require("util");
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -8,17 +9,25 @@ const server = http.createServer((req, res) => {
     res.end("Ceci est un overview");
   } else if (pathName === "/product") {
     res.end("Ceci est le produit");
-  }else{
-    res.writeHead(404,{
-        'Context-type':'text/html'
-    }) //On ajoute un status au niveau du retour de message. 
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`,"utf-8", (err, data) => {
+      if (err) throw err;
+      let jsonData=JSON.parse(data)
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(jsonData));
+   
+    });
+  } else {
+    res.writeHead(404, {
+      "Context-type": "text/html",
+    }); //On ajoute un status au niveau du retour de message.
     //Dans ce cas, il y'aura une entête qui sera crée. Qui aura comme statut 404
     //On définit aussi une entete dans l'erreur, qui est une pièce de reponse qqu'on envoie.
-    res.end('<h1>Page Not Found</h1>')
+    res.end("<h1>Page Not Found</h1>");
   }
 
-  console.log(req.url); //Cette méthode retourne l'Url qu'on passe en paramètre
-  res.end("Hello World from server");
+  // console.log(req.url); //Cette méthode retourne l'Url qu'on passe en paramètre
+  // res.end("Hello World from server");
 });
 
 server.listen(8000, "127.0.0.1", () => {
