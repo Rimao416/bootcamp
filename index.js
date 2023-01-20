4a686ef8146e3a826322ba33ed974b979a46c142
Rimao416 <omarkayumba12345@gmail.com>
2023-01-20 12:45:46 +0100

    Debut de l'express js

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-const util = require("util");
+const express=require("express") //Faire appel au Package d'expressJs
+const app=express() 
 
-const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("Ceci est un overview");
-  } else if (pathName === "/product") {
-    res.end("Ceci est le produit");
-  } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`,"utf-8", (err, data) => {
-      if (err) throw err;
-      let jsonData=JSON.parse(data)
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(jsonData));
-   
-    });
-  } else {
-    res.writeHead(404, {
-      "Context-type": "text/html",
-    }); //On ajoute un status au niveau du retour de message.
-    //Dans ce cas, il y'aura une entête qui sera crée. Qui aura comme statut 404
-    //On définit aussi une entete dans l'erreur, qui est une pièce de reponse qqu'on envoie.
-    res.end("<h1>Page Not Found</h1>");
-  }
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"Hello from server"})
+})
 
-  // console.log(req.url); //Cette méthode retourne l'Url qu'on passe en paramètre
-  // res.end("Hello World from server");
-});
+app.post('/',(req,res)=>{
+    res.send('You can post here everything you want')
+})
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Le serveur est lancé");
-});
+const port=3000
+app.listen(port,()=>{
+    console.log(`App running on port ${port}`)
+})
