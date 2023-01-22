ff99702512270a02ce6ca9ed4ccb9110f0c11ec0
Rimao416 <omarkayumba12345@gmail.com>
2023-01-22 12:46:42 +0100

    Ajout des variables environnements

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const morgan = require("morgan");
 const tourRouter=require('./routes/tourRoutes')
 const userRouter=require("./routes/userRoutes")
 const app = express();
+
+
+
 // 1) MIDDLEWARE
-app.use(morgan("dev"));
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
