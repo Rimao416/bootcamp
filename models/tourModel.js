55dd7b3ecc76b146ec50a853714990bbd575b26b
Rimao416 <omarkayumba12345@gmail.com>
2023-02-04 23:31:49 +0100

    Import-dev-data

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,62 @@
-const mongoose=require("mongoose")
-const tourSchema=new mongoose.Schema({
-    name:{
-      type:String,
-      required:[true,"Une tour se doit d'avoir un nom"],
-      unique:true
+const mongoose = require("mongoose");
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Une tour se doit d'avoir un nom"],
+    unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must be a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A group must a group size"],
+  },
+  difficulty: {
+    type: String,
+    // required: [true, "A tour should have a difficulty"],
+    enum: {
+      values: ["easy", "medium", "difficult"],
+      message: "Parmi ces 3 seulement",
     },
-    rating:{
-      type:Number,
-      default:4.5,
-    },
-    price:{
-      type:Number,
-      required:[true, "Une tour doit avoir un prix"]
-    }
-  })
-  const Tour=mongoose.model('Tour',tourSchema)
-  module.exports=Tour;
-  
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, "Rating must have above 1.0"],
+    max: [5, "Rating must have  below 5.0"],
+  },
+  ratingsQuantity: { type: Number, default: 0 },
+  price: {
+    type: Number,
+    required: [true, "Une tour doit avoir un prix"],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour should have a Summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a images"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  },
+});
+const Tour = mongoose.model("Tour", tourSchema);
+module.exports = Tour;
