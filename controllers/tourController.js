c707b7d7089271f43b1b9dfed4b46e46c4c28bb0
Rimao416 <omarkayumba12345@gmail.com>
2023-02-05 10:45:45 +0100

    Cheapeast Tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require("./../models/tourModel");
 
+
+exports.aliasTopTours=(req,res,next)=>{
+  req.query.limit='5'
+  req.query.sort='-ratingsAverage,price',
+  req.query.fields='name,price,ratingsAverage,summary,difficulty'
+  next()
+}
 exports.getTour = async (req, res) => {
   try {
     // First Way
