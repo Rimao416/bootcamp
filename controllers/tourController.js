12f935a2cc255751ce73ac0daffafaba674928a3
Rimao416 <omarkayumba12345@gmail.com>
2023-02-05 10:17:24 +0100

    Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -100,6 +100,13 @@ exports.getAllTours = async (req, res) => {
       }else{
         query=query.sort('-createdAt')
       }
+      // 3) LIMITNG FIELDS
+      if(req.query.fields){
+        const fields=req.query.fields.split(',').join(' ')
+        query=query.select(fields)
+      }else{
+        query=query.select('-__v')
+      }
       console.log(query)
 
     // {difficulty:'easy', duration :{$gt:5}}
