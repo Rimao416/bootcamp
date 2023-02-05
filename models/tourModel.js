8b2dff6ba54445ab8cb79e1d5b002e68e3490282
Rimao416 <omarkayumba12345@gmail.com>
2023-02-05 15:18:04 +0100

    AgregationDocumentDocumentMiddleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,71 +1,108 @@
 const mongoose = require("mongoose");
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Une tour se doit d'avoir un nom"],
-    unique: true,
-    trim: true,
-  },
-  duration: {
-    type: Number,
-    required: [true, "A tour must be a duration"],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "A group must a group size"],
-  },
-  difficulty: {
-    type: String,
-    // required: [true, "A tour should have a difficulty"],
-    enum: {
-      values: ["easy", "medium", "difficult"],
-      message: "Parmi ces 3 seulement",
+const slugify = require("slugify");
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Une tour se doit d'avoir un nom"],
+      unique: true,
+      trim: true,
+    },
+    slug:String,
+    duration: {
+      type: Number,
+      required: [true, "A tour must be a duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A group must a group size"],
+    },
+    difficulty: {
+      type: String,
+      // required: [true, "A tour should have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Parmi ces 3 seulement",
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must have above 1.0"],
+      max: [5, "Rating must have  below 5.0"],
+    },
+    ratingsQuantity: { type: Number, default: 0 },
+    price: {
+      type: Number,
+      required: [true, "Une tour doit avoir un prix"],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A tour should have a Summary"],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a images"],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
     },
   },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-    min: [1, "Rating must have above 1.0"],
-    max: [5, "Rating must have  below 5.0"],
-  },
-  ratingsQuantity: { type: Number, default: 0 },
-  price: {
-    type: Number,
-    required: [true, "Une tour doit avoir un prix"],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, "A tour should have a Summary"],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, "A tour must have a images"],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select:false
-  },
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-},{
-  toJSON:{virtuals:true},
-  toObject:{virtuals:true}
+);
+
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
 });
 
-tourSchema.virtual('durationWeeks').get(function(){
-  return this.duration/7
+// DOCUMENT MIDDLEWARE : runs before .save() and create()
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next()
+});
+
+// tourSchema.pre("save",function(next){
+//   console.log("Entrain de persister")
+//   next()
+// })
+
+// tourSchema.post("save",function(doc,next){
+//     console.log(doc)
+//     next()
+// })
+
+// QUERY MIDDLEWARE
+
+tourSchema.pre(/^find/,function(next){
+  // Your logic here
+  this.find({secretTour:{$ne:true}})
+  next()
 })
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 module.exports = Tour;
