cd6966b608eb23e52a69f3660ea62d5befff580e
Rimao416 <omarkayumba12345@gmail.com>
2023-02-06 13:55:28 +0100

    Refactor Global Handle Error

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express"); //Faire appel au Package d'expressJs
 const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const app = express();
@@ -31,25 +33,10 @@ app.use("/api/v1/users", userRouter);
 // Handle Errors
 
 app.all("*", (req, res, next) => {
-  // res.status(404).json({
-  //   status:'fail',
-  //   message:`Can't find ${req.originalUrl} on this server`
-  // })
-  const err = new Error(`Can't find ${req.originalUrl} on this Server`);
-  err.status = "Fail";
-  err.statusCode = 404;
-  next(err)
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 // 4) SERVER
 
