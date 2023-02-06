55c5a2aa3400458327135fbaaafbe647e1b3316c
Rimao416 <omarkayumba12345@gmail.com>
2023-02-06 05:16:46 +0100

    Gestion routes 404

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const userRouter=require("./routes/userRoutes")
 const app = express();
 
 
-
+ 
 // 1) MIDDLEWARE
 if(process.env.NODE_ENV==='development'){
   app.use(morgan("dev"));
@@ -13,10 +13,6 @@ if(process.env.NODE_ENV==='development'){
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-  console.log("Hello from middleware");
-  next();
-});
 app.use((req, res, next) => {
   req.requestTime = new Date();
   next();
@@ -38,6 +34,12 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+app.all('*',(req,res,next)=>{
+  res.status(404).json({
+    status:'fail',
+    message:`Can't find ${req.originalUrl} on this server`
+  })
+})
 // 4) SERVER
 
 module.exports=app;
