6d7676c2ea0a5e2e8fc2bd734287126df637b5dc
Rimao416 <omarkayumba12345@gmail.com>
2023-02-07 11:07:04 +0100

    uncaughtException

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./index");
 dotenv.config({ path: "./config.env" });
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION");
+  process.exit(1);
+});
+
 // console.log(process.env)
 
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-mongoose.set('strictQuery',true)
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -18,19 +23,19 @@ mongoose
     // console.log(con.connections);
     // console.log(con);
     console.log("Connexion réussie");
-  })
-
+  });
 
 const port = process.env.PORT || 3000;
 
-const server=app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-process.on('unhandledRejection',err=>{
-  console.log(err.name,err.message)
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
   // process.exit(1)
-  server.close(()=>{
-    process.exit(1)
-  })
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
+console.log(x);
