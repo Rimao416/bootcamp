9c7e9beff1ec3960d7c120756c1e873c8e881bcd
Rimao416 <omarkayumba12345@gmail.com>
2023-02-07 10:59:57 +0100

    unhandledRejection

--- server.js
+++ server.js
@@ -18,11 +18,19 @@ mongoose
     // console.log(con.connections);
     // console.log(con);
     console.log("Connexion réussie");
-  });
+  })
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server=app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+process.on('unhandledRejection',err=>{
+  console.log(err.name,err.message)
+  // process.exit(1)
+  server.close(()=>{
+    process.exit(1)
+  })
+})
