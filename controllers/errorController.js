7cf4b23bd8bc769eee940a059085e5fd76f6678d
Rimao416 <omarkayumba12345@gmail.com>
2023-02-07 10:29:40 +0100

    HandleCastErrorDB

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,47 @@
-module.exports=(err, req, res, next) => {
-    // console.log(err.stack)
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-  
+const AppError = require("./../utils/appError");
+const handleCastErrorDB = (err) => {
+  const message = `Invalid Id ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+  } else {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "Something get wrong",
+    });
   }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack)
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    // let error = { ...err }; VERSION ORIDGINALE
+    let error = err; //Version qui marche
+    console.log(error);
+    console.log(error);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
+};
