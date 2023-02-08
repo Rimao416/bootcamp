7a16700b0663853692132678e5726f64808313d9
Rimao416 <omarkayumba12345@gmail.com>
2023-02-08 09:11:52 +0100

    CreateNewUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("./../controllers/userController");
-
+const authController = require("./../controllers/authController");
 // CONTROLLERS
 
+router.post('/signup',authController.signup)
 router
   .route("/")
   .get(userController.getAllUsers)
