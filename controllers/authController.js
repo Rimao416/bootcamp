c97656e4eaaef04b8073a21bb4463b080caf0b64
Rimao416 <omarkayumba12345@gmail.com>
2023-02-09 11:25:08 +0100

    Authorisation

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,6 +77,15 @@ exports.protect = catchAsync(async (req, res, next) => {
       401
     );
   }
-
+  req.user = freshUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError("You are not allow to execute this", 403));
+    }
+    next();
+  };
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,13 +13,17 @@ router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router.route("/tour-stats").get(tourController.getTourStats);
 router
   .route("/")
-  .get(authController.protect,tourController.getAllTours)
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
   .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo("admin","lead-guide"),
+    tourController.deleteTour
+  );
 
 module.exports = router;
