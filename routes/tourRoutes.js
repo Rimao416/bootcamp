f2cab16945b8d3497b2e1ed727f234c001bea107
Rimao416 <omarkayumba12345@gmail.com>
2023-02-28 08:50:57 +0100

    NestedRoutes2

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("./../controllers/authController");
-const reviewController = require("./../controllers/reviewController");
 const tourController = require("./../controllers/tourController");
+const reviewRouter = require("./../routes/reviewRoutes");
 
 // router.param('id',tourController.checkId)
 
+router.use('/:tourId/reviews',reviewRouter)
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -22,20 +23,18 @@ router
   .patch(tourController.updateTour)
   .delete(
     authController.protect,
-    authController.restrictTo("admin","lead-guide"),
+    authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour
   );
 
-  // Nested Routes
+// Nested Routes
 
-router
-.route("/:tourId/reviews")
-.post(
-  authController.protect,
-  authController.restrictTo("users"),
-  reviewController.createReview
-);
-
-
+// router
+// .route("/:tourId/reviews")
+// .post(
+//   authController.protect,
+//   authController.restrictTo("users"),
+//   reviewController.createReview
+// );
 
 module.exports = router;
